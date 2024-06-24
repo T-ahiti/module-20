2e967e4a6be159f8648ecff0727f47f19ce29460
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-25 00:59:12 +0600

    4th commit

--- inch-to-feet.js
+++ inch-to-feet.js
@@ -1,8 +1,17 @@
-const myInches =12;
-const myFeet = myInches/12;
-console.log(myFeet)
 
+// const myInches =12;
+// const myFeet = myInches/12;
+// console.log(myFeet)
+// 
+// const DadaInches =142;
+// const DadaFeet = DadaInches/12;
+// console.log(DadaFeet)
 
-const DadaInches =142;
-const DadaFeet = DadaInches/12;
-console.log(DadaFeet)
+function inchToFeet(inches){
+     const feet = inches/12;
+     return feet;
+}
+
+const dadaInches =144;
+const dadaFeet = inchToFeet(dadaInches)
+console.log(dadaFeet)
