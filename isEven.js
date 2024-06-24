5fbfb6b45ba0f7e0e9986074f88a596048d4ee18
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-25 01:22:07 +0600

    third commit

--- isEven.js
+++ isEven.js
@@ -1,6 +1,17 @@
-function isEven (number){
+
+function isEven (number){   
     const reminder = number % 2;
-    console.log(reminder)
+   
+    if(reminder === 0){
+        // console.log('number is even')
+        return true;
+    }
+    else{
+        // console.log('number is odd')
+        return false;
+    }
 }
 
-isEven(45)
+// isEven(45)
+const myNumber = isEven(303)
+console.log(myNumber)
