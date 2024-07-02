776abcccb2aca3f0fb664b979a0d815cb3961efe
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-02 21:05:49 +0600

    2nd try

--- findOddSum.js
+++ findOddSum.js
@@ -14,4 +14,18 @@
 // const myNumbers = [12, 56, 37, 12, 55, 23]
 // getSumOfArray(myNumbers)
 
-function getSumOfArray
+function getSumOfArray(numbers){
+
+    let sum = 0;
+    for( i = 0; i<numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        sum = sum + element;
+        console.log(sum);
+    }
+    return sum;
+}
+
+
+const mynumbers = [243, 34, 43, 95, 84, 30, 39];
+getSumOfArray(mynumbers);
