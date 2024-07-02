1263b371cdcb49c6b81462131d08f5576939dfc9
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-02 20:50:31 +0600

    get sum added

---
+++ findOddSum.js
@@ -0,0 +1,17 @@
+
+// function getSumOfArray(numbers){
+// 
+    // let sum = 0;
+    // for(let i=0; i< numbers.length; i++){
+        // const index = i;
+        // const element = numbers[index];
+        // sum = sum + element;
+        // console.log(index, element, sum);
+    // }
+    // return sum;
+// }
+// 
+// const myNumbers = [12, 56, 37, 12, 55, 23]
+// getSumOfArray(myNumbers)
+
+function getSumOfArray
