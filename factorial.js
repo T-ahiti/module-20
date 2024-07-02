7f287730e988419824e7b442d47b04ca8a206b1f
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-02 23:13:29 +0600

    second commit

--- factorial.js
+++ factorial.js
@@ -5,3 +5,14 @@
     // console.log(i, sum)
 // }
 
+function sumOfNumbers(number){
+    let sum = 1;
+    for ( let i = 1; i<=number; i++){
+       sum = sum * i;
+       console.log(i, sum)
+    }
+    return sum;
+}
+
+const sum  = sumOfNumbers(7)
+console.log(sum)
