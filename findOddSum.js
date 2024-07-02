7165ba667748a817036a73c346826e3c1b2ec2a2
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-02 21:27:16 +0600

    fourth commit

--- findOddSum.js
+++ findOddSum.js
@@ -14,18 +14,21 @@
 // const myNumbers = [12, 56, 37, 12, 55, 23]
 // getSumOfArray(myNumbers)
 
-function getSumOfArray(numbers){
-
-    let sum = 0;
-    for( i = 0; i<numbers.length; i++){
+function getOddSumOfArray(numbers){
+    const oddNumbers = [];
+    for( let i = 0; i<numbers.length; i++){
         const index = i;
         const element = numbers[index];
-        sum = sum + element;
-        console.log(sum);
+        if(element % 2 !==0){
+            console.log(index);
+            oddNumbers.push(element);
+        }
+
     }
-    return sum;
+    return oddNumbers;
 }
 
 
 const mynumbers = [243, 34, 43, 95, 84, 30, 39];
-getSumOfArray(mynumbers);
+const oddNumbers = getOddSumOfArray(mynumbers);
+console.log(oddNumbers)
