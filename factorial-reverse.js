38bc043f84a3e27557240fbdd2ebaea64b823389
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-02 23:51:07 +0600

    third commit

--- factorial-reverse.js
+++ factorial-reverse.js
@@ -1,1 +1,14 @@
 
+
+function factorial (number){
+    let sum = 1;
+    for(let i=7; i>=number ;i--){
+        sum = sum * i;
+        console.log(i, sum)
+    }
+
+    return sum;
+}
+
+const sum  = factorial(1)
+console.log(sum)
